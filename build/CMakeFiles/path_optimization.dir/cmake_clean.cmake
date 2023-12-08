ab74e5ceea9be0a3b33171202bf42d7f3960c6da
clock <1162411462@qq.com>
2023-12-08 17:36:40 +0800

    数据稀疏优化,同时考虑了直线与斜线情况,大幅度删减无用数据

---
+++ build/CMakeFiles/path_optimization.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/path_optimization.dir/my_optimization.cpp.o"
+  "path_optimization"
+  "path_optimization.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/path_optimization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
